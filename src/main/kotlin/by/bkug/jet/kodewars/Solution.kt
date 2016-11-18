b7f271bb02ef5f9cdf9d6caf3b1defedcb82e299
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-11-18 10:45:04 +0300

    Kodewars Init.

---
+++ src/main/kotlin/by/bkug/jet/kodewars/Solution.kt
@@ -0,0 +1,6 @@
+package by.bkug.jet.kodewars
+
+fun solution(arg1: Int, arg2: Int): Int {
+    return arg1 + arg2
+}
+
---
+++ src/test/kotlin/by/bkug/jet/kodewars/SolutionTest.kt
@@ -0,0 +1,10 @@
+package by.bkug.jet.kodewars
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SolutionTest {
+    @Test fun solve() {
+        assertEquals(2, solution(1, 1))
+    }
+}
