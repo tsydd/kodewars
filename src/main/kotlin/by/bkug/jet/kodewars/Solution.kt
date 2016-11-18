12ad3567f3338111f3918dff193614ddd5d1b243
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-11-18 11:04:09 +0300

    War one.

--- src/main/kotlin/by/bkug/jet/kodewars/Solution.kt
+++ src/main/kotlin/by/bkug/jet/kodewars/Solution.kt
@@ -4,3 +4,22 @@ fun solution(arg1: Int, arg2: Int): Int {
     return arg1 + arg2
 }
 
+
+/**
+ * Description:
+ * Take 2 strings s1 and s2 including only letters from a to z.
+ * Return a new sorted string, the longest possible, containing distinct letters,
+ * - each taken only once - coming from s1 or s2.
+ *
+ * Examples:
+ *
+ * a = "xyaabbbccccdefww"
+ * b = "xxxxyyyyabklmopq"
+ * longest(a, b) -> "abcdefklmopqwxy"
+ *
+ * a = "abcdefghijklmnopqrstuvwxyz"
+ * longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+ */
+fun longest(s1: String, s2: String): String {
+    return "TODO";
+}
--- src/test/kotlin/by/bkug/jet/kodewars/SolutionTest.kt
+++ src/test/kotlin/by/bkug/jet/kodewars/SolutionTest.kt
@@ -4,7 +4,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SolutionTest {
-    @Test fun solve() {
+    @Test fun solveTest() {
         assertEquals(2, solution(1, 1))
+    }
+
+    @Test fun longestTest() {
+        assertEquals("aehrsty", longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", longest("inmanylanguages", "theresapairoffunctions"));
     }
 }
